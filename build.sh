18dda2f54790371064c8b77ff53d5cb40b892eec
hvalev <hvalev@yahoo.de>
2024-11-26 00:05:33 +0100

    added hx711 lib and updated build script

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ docker run --rm -v $HOME:$HOME -u $UID -w $PWD/micropython hvalev/esp-open-sdk m
 cp ./libs/micropython-mqtt/mqtt_as/mqtt_as.py "./micropython/ports/esp8266/modules/mqtt_as.py"
 cp ./libs/MicroPython_Statistics/statistics.py "./micropython/ports/esp8266/modules/statistics.py"
 cp ./libs/hamqa/hamqa/hamqa_mpy.py "./micropython/ports/esp8266/modules/hamqa.py"
+cp ./libs/hx711/hx711_gpio.py "./micropython/ports/esp8266/modules/hx711.py"
 docker run --rm -v $HOME:$HOME -u $UID -w $PWD/micropython/ports/esp8266 hvalev/esp-open-sdk make -j BOARD=ESP8266_GENERIC
 cp ./micropython/ports/esp8266/build-ESP8266_GENERIC/firmware.* ./

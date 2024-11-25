57a198baf2de491df03232e3bf6bdc096b9a59ce
hvalev <hvalev@yahoo.de>
2024-11-26 00:02:51 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ git submodule update --init --recursive --progress ./micropython
 docker run --rm -v $HOME:$HOME -u $UID -w $PWD/micropython hvalev/esp-open-sdk make -C mpy-cross
 cp ./libs/micropython-mqtt/mqtt_as/mqtt_as.py "./micropython/ports/esp8266/modules/mqtt_as.py"
 cp ./libs/MicroPython_Statistics/statistics.py "./micropython/ports/esp8266/modules/statistics.py"
+cp ./libs/hamqa/hamqa/hamqa_mpy.py "./micropython/ports/esp8266/modules/hamqa.py"
 docker run --rm -v $HOME:$HOME -u $UID -w $PWD/micropython/ports/esp8266 hvalev/esp-open-sdk make -j BOARD=ESP8266_GENERIC
+cp ./micropython/ports/esp8266/build-ESP8266_GENERIC/firmware.* ./
